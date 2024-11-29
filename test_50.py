fbac9139203f28ad8bdf717a3463e6088a10be99
Eros God <erosgod19921118@gmail.com>
2024-11-29 03:34:32 -0500

    Add test_failures feature by FMRGJ.

--- test_50.py
+++ test_50.py
@@ -2,6 +2,5 @@ import pytest
 
 @pytest.mark.parametrize("i", range(50))
 def test_sum(i):
-    # if i == 25:
-    #     pytest.fail("bad luck")
-    pass
+    if i in (17, 25):
+        pytest.fail("bad luck")
